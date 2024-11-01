85ee938ff428f7949905ce2a9163d31945aad5bf
Jiang Xiaolan <jiang.x@ctw.inc>
2024-11-01 17:23:23 +0900

    update

--- agent.py
+++ agent.py
@@ -9,13 +9,14 @@ import os
 import uuid
 import shutil
 
-from math import floor
-from typing import Optional, List, Dict, Any
+from typing import Optional, List
 import torch
 import gradio as gr
-from transformers import pipeline
-from transformers.pipelines.audio_utils import ffmpeg_read
-
+# from transformers import pipeline
+# from transformers.pipelines.audio_utils import ffmpeg_read
+# from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperFeatureExtractor, WhisperTokenizer
+import librosa
 
 # configuration
 MODEL_NAME = "federerjiang/mambavoice-ja-v1"
@@ -23,27 +24,37 @@ BATCH_SIZE = 4
 CHUNK_LENGTH_S = 15
 FILE_LIMIT_MB = 1000
 TOKEN = os.environ.get('HF_TOKEN', None)
-# device setting
-if torch.cuda.is_available():
-    torch_dtype = torch.bfloat16
-    device = "cuda:0"
-else:
-    torch_dtype = torch.float32
-    device = "cpu"
-
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 
-# define the pipeline
-pipe = pipeline(
-    model=MODEL_NAME,
-    chunk_length_s=CHUNK_LENGTH_S,
-    batch_size=BATCH_SIZE,
-    torch_dtype=torch_dtype,
-    device=device,
-    trust_remote_code=True,
-    token=TOKEN,
-)
+# device setting
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
+model = WhisperForConditionalGeneration.from_pretrained(MODEL_NAME, token=TOKEN, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
+model.generation_config.language = "japanese"
+model.generation_config.task = "transcribe"
+model.generation_config.forced_decoder_ids = None
+model.config.apply_spec_augment = False
+# optimize decoding strategy
+model.generation_config.length_penalty = 0
+model.generation_config.num_beams = 3
+model.eval()
+model.to(device)
+
+processor = WhisperProcessor.from_pretrained(MODEL_NAME, token=TOKEN, torch_dtype=torch_dtype, language="japanese", task="transcribe")
+feature_extractor = WhisperFeatureExtractor.from_pretrained(MODEL_NAME, token=TOKEN, torch_dtype=torch_dtype)
+tokenizer = WhisperTokenizer.from_pretrained(MODEL_NAME, token=TOKEN, torch_dtype=torch_dtype, language="japanese", task="transcribe")
+
+# # define the pipeline
+# pipe = pipeline(
+#     model=MODEL_NAME,
+#     chunk_length_s=CHUNK_LENGTH_S,
+#     batch_size=BATCH_SIZE,
+#     torch_dtype=torch_dtype,
+#     device=device,
+#     trust_remote_code=True,
+#     token=TOKEN,
+# )
 
 @dataclass
 class SilenceSegment:
@@ -282,26 +293,109 @@ class AudioSplitter:
                 print(f"Segment {i}: {seg.duration:.2f}s")
 
 
-def get_prediction(inputs, prompt: Optional[str]=None):
-    generate_kwargs = {
-        "language": "japanese", 
-        "task": "transcribe",
-        "length_penalty": 0,
-        "num_beams": 2,
-    }
-    if prompt:
-        generate_kwargs['prompt_ids'] = pipe.tokenizer.get_prompt_ids(prompt, return_tensors='pt').to(device)
-    prediction = pipe(inputs, return_timestamps=True, generate_kwargs=generate_kwargs)
-    text = "".join([c['text'] for c in prediction['chunks']])
-    return text
+def change_volume(y, target_rms=0.1):
+    # Calculate the RMS value
+    rms = np.sqrt(np.mean(y**2))
+    # Calculate the scaling factor
+    scaling_factor = target_rms / rms
+    print(f"Scaling factor: {scaling_factor}")
+    # Apply the scaling factor
+    normalized_y = y * scaling_factor
+    return normalized_y
 
+
+def add_silence(audio, sr, duration=0.1):
+    # duration 表示需要添加的静音长度，单位为秒
+    silence = np.zeros(int(sr * duration))
+    padded_audio = np.concatenate((silence, audio))
+    return padded_audio
+
+
+def dynamic_silence_threshold(audio_array, sample_rate, std_multiplier=0.75):
+    """
+    Calculate silence length with a dynamic threshold based on the audio's statistics.
+
+    Parameters:
+    - audio_array: numpy array of audio samples.
+    - sample_rate: Sampling rate of the audio.
+    - std_multiplier: Multiplier for the standard deviation to set the silence threshold.
+
+    Returns:
+    - silence_length: Length of the initial silence in seconds.
+    """
+    # Calculate the mean and standard deviation of the audio amplitude
+    mean_amplitude = np.mean(np.abs(audio_array))
+    std_amplitude = np.std(np.abs(audio_array))
+    print(f"Mean amplitude: {mean_amplitude}, Std amplitude: {std_amplitude}")
+    # Determine the silence threshold dynamically
+    silence_threshold = mean_amplitude + (std_amplitude * std_multiplier)
+    # Find the first index where the audio amplitude exceeds the dynamic silence threshold
+    silence_end_indices = np.where(np.abs(audio_array) > silence_threshold)[0]
+    if len(silence_end_indices) == 0:
+        # If the entire audio is below the threshold, consider it all silence
+        silence_end_index = len(audio_array)
+    else:
+        silence_end_index = silence_end_indices[0]
+    # Convert the index to time in seconds
+    silence_length = silence_end_index
+    return silence_length, silence_threshold
+
+
+def inference(model, waveform):
+    # waveform = waveform.astype(np.float16)
+    input_features = processor(waveform, sampling_rate=16000, return_tensors="pt", device=device).input_features
+    input_features = input_features.to(torch_dtype)
+    predicted_ids = model.generate(input_features.to(device), return_dict_in_generate=True, output_logits=True)
+    transcription = processor.batch_decode(predicted_ids["sequences"], skip_special_tokens=True)
+    return transcription[0]
+
+# def get_prediction(inputs, prompt: Optional[str]=None):
+#     generate_kwargs = {
+#         "language": "japanese", 
+#         "task": "transcribe",
+#         "length_penalty": 0,
+#         "num_beams": 2,
+#     }
+#     if prompt:
+#         generate_kwargs['prompt_ids'] = pipe.tokenizer.get_prompt_ids(prompt, return_tensors='pt').to(device)
+#     prediction = pipe(inputs, return_timestamps=True, generate_kwargs=generate_kwargs)
+#     text = "".join([c['text'] for c in prediction['chunks']])
+#     return text
 
 def generate_random_folder():
     """Generate a random folder name using UUID"""
     return str(uuid.uuid4())
 
 
-def transcribe(inputs: str):
+# def transcribe_v1(inputs: str):
+#     if inputs is None:
+#         raise gr.Error("音声ファイルが送信されていません！リクエストを送信する前に、音声ファイルをアップロードまたは録音してください。")
+#     splitter = AudioSplitter(
+#         min_segment_length=2.0,
+#         max_segment_length=10.0,
+#         vad_threshold=0.3,
+#         min_silence_duration=0.5,
+#         padding_duration=0.5
+#     )
+
+#     tmp_folder = generate_random_folder()
+#     output_files = splitter.split_audio(inputs, output_dir=tmp_folder)
+
+#     outputs = []
+#     for i, output_file in enumerate(output_files):
+#         with open(output_file, "rb") as f:
+#             inputs = f.read()
+#             inputs = ffmpeg_read(inputs, pipe.feature_extractor.sampling_rate)
+#             inputs = {"array": inputs, "sampling_rate": pipe.feature_extractor.sampling_rate}
+#             outputs.append(get_prediction(inputs))
+#             print(f"Segment {i}: {outputs[-1]}")
+#             torch.cuda.empty_cache()
+    
+#     shutil.rmtree(tmp_folder)
+#     return "".join(outputs)
+
+
+def transcribe_v2(inputs: str):
     if inputs is None:
         raise gr.Error("音声ファイルが送信されていません！リクエストを送信する前に、音声ファイルをアップロードまたは録音してください。")
     splitter = AudioSplitter(
@@ -309,7 +403,7 @@ def transcribe(inputs: str):
         max_segment_length=10.0,
         vad_threshold=0.3,
         min_silence_duration=0.5,
-        padding_duration=0.5
+        padding_duration=0.01
     )
 
     tmp_folder = generate_random_folder()
@@ -317,13 +411,16 @@ def transcribe(inputs: str):
 
     outputs = []
     for i, output_file in enumerate(output_files):
-        with open(output_file, "rb") as f:
-            inputs = f.read()
-            inputs = ffmpeg_read(inputs, pipe.feature_extractor.sampling_rate)
-            inputs = {"array": inputs, "sampling_rate": pipe.feature_extractor.sampling_rate}
-            outputs.append(get_prediction(inputs))
-            print(f"Segment {i}: {outputs[-1]}")
-            torch.cuda.empty_cache()
+        waveform, _ = librosa.load(output_file, sr=16000)
+        waveform = change_volume(waveform, target_rms=0.1)
+        silence_length, silence_threshold = dynamic_silence_threshold(waveform, 16000)
+        duration = min(max(0.01, (2600 - silence_length) / 16000), 0.1)
+        print(f"Silence Length: {silence_length}, Duration: {duration}")
+        waveform = add_silence(waveform, 16000, duration=duration)
+        result = inference(model, waveform)
+        outputs.append(result)
+        print(f"Segment {i}: {outputs[-1]}")
+        torch.cuda.empty_cache()
     
     shutil.rmtree(tmp_folder)
     return "".join(outputs)
